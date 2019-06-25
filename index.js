15cd88758ce3787a6b40bae349593fa0958553dc
Ping Ping <ping.ping.lu@hotmail.com>
2019-06-25 15:37:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+console.log(happyHolidays)
+
+
+function happyHolidaysTo (name) {
+  return `Happy holidays, ${name}!`
+}
+var name =
